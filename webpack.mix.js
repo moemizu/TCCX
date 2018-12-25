ee1c0abaf418a242c72016b1c37ba42d3d7fd960
monikuri <pavat.tangmo@hotmail.com>
2018-12-25 17:44:29 +0700

    TH Sarabun New web font

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .extract(['lodash', 'jquery', 'bootstrap', 'popper.js', 'vue'])
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/fonts', 'public/fonts');
