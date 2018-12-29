6a314be18ebb3cf6ab8297947834716b567f2f9f
monikuri <pavat.tangmo@hotmail.com>
2018-12-29 10:52:07 +0700

    Change logo in the navigation bar

--- resources/views/tccx/nav.blade.php
+++ resources/views/tccx/nav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-tccx mb-3">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            <img class="d-inline-block align-top" src="/storage/images/tccxlogo.png" width="32" height="32" alt="TCCX">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
